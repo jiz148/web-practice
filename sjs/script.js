17744d58a63840bc9f8103c264e08b3095b97425
jiz148 <jizjiz148148@gmail.com>
2020-11-17 17:38:08 +0800

    modified js

--- sjs/script.js
+++ sjs/script.js
@@ -1,8 +1,24 @@
-sayHello = function () {
-    if (document.querySelector("#helloContainer").classList.contains("invisible")) {
-        document.querySelector("#helloContainer").classList.remove("invisible")
+document.addEventListener("DOMContentLoaded",
+    function (event) {
+        function sayHello (event) {
+            this.textContent = "Said It!!";
+            if (document.querySelector("#helloContainer").classList.contains("invisible")) {
+                document.querySelector("#helloContainer").classList.remove("invisible");
+            }
+            else {
+                document.querySelector("#helloContainer").classList.add("invisible");
+                this.textContent = "Say Hello";
+            }
+        }
+        document.querySelector("#helloButton").addEventListener("click", sayHello);
+        let count = 0;
+        document.addEventListener("touchend",
+            function (event) {
+                count += 1;
+                if (count === 15) {
+                    document.querySelector("#header-nav a span").textContent = "I love Ning!";
+                }
+            }
+            );
     }
-    else {
-        document.querySelector("#helloContainer").classList.add("invisible")
-    }
-}
+)
