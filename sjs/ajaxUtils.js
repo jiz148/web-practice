7446e53902d94ee9e2050a9f87a5b3138860800b
jiz148 <jizjiz148148@gmail.com>
2020-11-18 20:51:34 +0800

    added ajax and data

---
+++ sjs/ajaxUtils.js
@@ -0,0 +1,48 @@
+(function(global) {
+    let ajaxUtils = {};
+
+    function getRequestObject() {
+        if (global.XMLHttpRequest) {
+            return (new XMLHttpRequest());
+        }
+        else if (global.ActiveXObject) {
+            // For very old IE browsers (optional)
+            return (new ActiveXObject("Microsoft.XMLHTTP"));
+        }
+        else {
+            global.alert("Ajax is not supported!");
+            return (null);
+        }
+    }
+
+    ajaxUtils.sendGetRequest =
+        function (requestUrl, responseHandler, isJsonResponse) {
+        let request = getRequestObject();
+        request.onreadystatechange = function () {
+            handleResponse(request, responseHandler, isJsonResponse);
+        }
+
+        request.open("GET", requestUrl, true);
+        request.send(null);
+    }
+
+    function handleResponse(request, requestHandler, isJsonResponse) {
+        if ((request.readyState == 4) && (request.status == 200)) {
+
+            //Default to isJsonResponse = ture
+            if (isJsonResponse == undefined) {
+                isJsonResponse = true;
+            }
+
+            if (isJsonResponse) {
+                requestHandler(JSON.parse(request.responseText));
+            }
+            else {
+                requestHandler(request.responseText);
+            }
+        }
+    }
+
+    global.$ajaxUtils = ajaxUtils;
+
+})(window);
--- sjs/script.js
+++ sjs/script.js
@@ -11,14 +11,36 @@ document.addEventListener("DOMContentLoaded",
             }
         }
         document.querySelector("#helloButton").addEventListener("click", sayHello);
+
         let count = 0;
         document.addEventListener("touchend",
             function (event) {
                 count += 1;
-                if (count === 15) {
+                if (count === 25) {
                     document.querySelector("#header-nav a span").textContent = "Love Ning!";
                 }
             }
-            );
+        );
+
+        document.querySelector("#wujiangButton").addEventListener("click",
+            function (event) {
+                this.textContent = "Generated!";
+                if (document.querySelector("#wujiangContainer").classList.contains("invisible")) {
+                    document.querySelector("#wujiangContainer").classList.remove("invisible");
+                }
+                else {
+                    document.querySelector("#wujiangContainer").classList.add("invisible")
+                    this.textContent = "Generate Wujiang";
+                }
+
+                // call the server
+                $ajaxUtils.sendGetRequest("data/wujiang.json",
+                    function (response) {
+                        document.querySelector("#wujiang-name").textContent = "名称：" + response.名称;
+                        document.querySelector("#wujiang-level").textContent = "等级：" + response.等级;
+                        document.querySelector("#wujiang-profession").textContent = "职业：" + response.职业;
+                    })
+            }
+        )
     }
 )
